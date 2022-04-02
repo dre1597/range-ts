a941556e4a87d1a87c8979ab7289760ae45b3cdc
André Pereira <andrepereira97@yahoo.com>
2022-04-01 22:12:56 -0300

    :sparkles: range in ts

---
+++ range.ts
@@ -0,0 +1,18 @@
+function range(start: number, end: number, step = 1) {
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (start < end) {
+        start = start + step;
+        return { value: start, done: false };
+      }
+      return { done: true, value: end };
+    },
+  };
+}
+
+for(const num of range(0, 100, 10)) {
+  console.log(num);
+}
